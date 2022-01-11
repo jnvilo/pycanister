c8983df0f80eb8063414fb7d734c13eccee0452b
Jason Viloria <jason.viloria@optiscangroup.com>
2022-01-11 12:17:16 +0200

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 PACKAGE="pycanister"
-VERSION="0.0.4"
+VERSION="0.0.5"
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
